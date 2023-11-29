7b547e1b4e60aaa204e03eeb42804c17ccb433f7
ne5oo <106486790+neo5oo@users.noreply.github.com>
2023-11-29 15:28:14 +0300

    Подключение API + реализация поиска фильмов

---
+++ src/components/Search.jsx
@@ -0,0 +1,33 @@
+import React from "react";
+
+class Search extends React.Component {
+  state = {
+    search: '',
+  }
+
+  handleKey = (event) => {
+    if (event.key === 'Enter') {
+        this.props.searchMovies(this.state.search);
+    }
+  }
+
+  render() {
+    return (
+      <div className="row">
+        <div className="input-field">
+          <input
+            placeholder="search"
+            type="search"
+            className="validate"
+            value={this.state.search}
+            onChange={(event) => this.setState({ search: event.target.value })}
+            onKeyDown={this.handleKey}
+          />
+          <button className="btn search-btn" onClick={() => this.props.searchMovies(this.state.search)}>Search</button>
+        </div>
+      </div>
+    );
+  }
+}
+
+export { Search };
--- src/layout/Main.jsx
+++ src/layout/Main.jsx
@@ -1,7 +1,36 @@
-function Main() {
+import React from "react";
+import { Movies } from "../components/Movies";
+import { Preloader } from "../components/Preloader";
+import { Search } from "../components/Search";
+
+class Main extends React.Component {
+  state = {
+    movies: [],
+  };
+
+  componentDidMount() {
+    fetch("http://www.omdbapi.com/?i=tt3896198&apikey=ce105bb5&s=avengers")
+      .then(response => response.json())
+      .then(data => this.setState({ movies: data.Search }));
+  }
+
+  searchMovies = (str) => {
+    fetch(`http://www.omdbapi.com/?i=tt3896198&apikey=ce105bb5&s=${str}`)
+      .then(response => response.json())
+      .then(data => this.setState({ movies: data.Search }));
+  }
+
+  render() {
+    const { movies } = this.state;
     return <main className="container content">
-        Hello from Movies App
-    </main>
+        <Search searchMovies={this.searchMovies} />
+        {movies.length ? (
+          <Movies movies={this.state.movies} />
+        ) : (
+          <Preloader />
+        )}
+      </main>;
+  }
 }
 
-export {Main}
+export { Main };
